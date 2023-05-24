468e69cad0c495a9a749623d5c1ad602c1f4a2c4
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-05-23 20:35:42 -0400

    added correct project title case

--- scripts/github.js
+++ scripts/github.js
@@ -1,4 +1,51 @@
 
+function properCapitalization(language) {
+  const languageMap = {
+      "javascript": "JavaScript",
+      "firebase": "FireBase",
+      "firestore": "FireStore",
+      "sql": "SQL",
+      "nosql": "NoSQL",
+      "mongodb": "MongoDB",
+      "flutter": "Flutter",
+      "react": "React",
+      "java": "Java",
+      "python": "Python",
+      "c++": "C++",
+      "cpp": "C++",
+      "c#": "C#",
+      "angular": "Angular",
+      "vue": "Vue.js",
+      "typescript": "TypeScript",
+      "ruby": "Ruby",
+      "ruby on rails": "Ruby on Rails",
+      "swift": "Swift",
+      "kotlin": "Kotlin",
+      "php": "PHP",
+      "dotnet": ".NET",
+      ".net": ".NET",
+      "html": "HTML",
+      "css": "CSS",
+      "shell": "Shell",
+      "markdown": "Markdown",
+      "vim" : "Vim",
+      "database" : "Database",
+      "website" : "Website",
+      "api" : "API",
+      "restful api" : "RESTful API",
+      "restful-api" : "RESTful API",
+      "ai" : "A.I.",
+      "machine learning" : "Machine Learning",
+      "game" : "Game",
+      "dynamodb" : "DynamoDB",
+      "dart": "Dart"
+  };
+
+  return languageMap[language.toLowerCase()] || language.toUpperCase();
+}
+
+
+
 
 async function fetchData() {
   try {
@@ -24,114 +71,135 @@ async function fetchData() {
         projectDescription.className = 'project-description';
         tags.className = 'tags';
         buttonWrapper.className = 'button-wrapper';
-        
         button.className = 'project-button';
+        
+
+
+
+        function toTitleCase(str) {
+          return str.replace(/\w\S*/g, function(word) {
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+          });
+        }
+
+
+        
+
+        data[i].topics.forEach(function(item) {
+          console.log(item);
+
+          let tag = document.createElement('span');
+          tag.className = 'tag';
+          tag.innerText = properCapitalization(item);
+          tags.appendChild(tag);
+
+          let backgroundColor;
+
+          switch (properCapitalization(item).toUpperCase()) {
+            case 'JAVASCRIPT':
+              backgroundColor = "#fdd692"; // Pastel yellow
+              break;
+            case 'PYTHON':
+              backgroundColor = "#ff9980"; // Pastel red
+              break;
+            case 'HTML':
+              backgroundColor = "#ffafcc"; // Pastel pink
+              break;
+            case 'C':
+              backgroundColor = "#7fc8f8"; // Pastel blue
+              break;
+            case 'C++':
+              backgroundColor = "#a1e1d4"; // Pastel green
+              break;
+            case 'JAVA':
+              backgroundColor = "#a2cffe"; // Pastel sky blue
+              break;
+            case 'RUBY':
+              backgroundColor = "#ff9b9b"; // Pastel salmon
+              break;
+            case 'CSS':
+              backgroundColor = "#c7ceea"; // Pastel lavender
+              break;
+            case 'PHP':
+              backgroundColor = "#ffb2a7"; // Pastel coral
+              break;
+            case 'SWIFT':
+              backgroundColor = "#ffd06e"; // Pastel yellow-orange
+              break;
+            case 'GO':
+              backgroundColor = "#b5ffd9"; // Pastel mint
+              break;
+            case 'RUST':
+              backgroundColor = "#c9c9c9"; // Pastel gray
+              break;
+            case 'TYPESCRIPT':
+              backgroundColor = "#b8e9e5"; // Pastel turquoise
+              break;
+            case 'KOTLIN':
+              backgroundColor = "#ffc0cb"; // Pastel rose
+              break;
+            case 'SCALA':
+              backgroundColor = "#f6b4ee"; // Pastel orchid
+              break;
+            case 'PERL':
+              backgroundColor = "#f9cb9c"; // Pastel apricot
+              break;
+            case 'HASKELL':
+              backgroundColor = "#c5ddeb"; // Pastel powder blue
+              break;
+            case 'LUA':
+              backgroundColor = "#ffc1a1"; // Pastel peach
+              break;
+            case 'R':
+              backgroundColor = "#e8b1b1"; // Pastel light pink
+              break;
+            case 'MATLAB':
+              backgroundColor = "#ffe5b4"; // Pastel beige
+              break;
+            default:
+              backgroundColor = "#ffffff"; // White (default)
+          }
+
+
+          tag.style.backgroundColor = backgroundColor;
+
+
+
+        });
+
+        
+
+
+
+
+
 
         // Add inner text
         projectTitle.innerText = data[i].name;
         projectDescription.innerText = data[i].description;
-        button.innerText = 'Visit Website';
-
-        // Create tags
-        try {
-          const response = await fetch(data[i].languages_url);
-          const tagData = await response.json();
-      
-          for (const key in tagData) {
-            if (tagData.hasOwnProperty(key)) {
-
-              let tag = document.createElement('span');
-              tag.className = 'tag';
-              tag.innerText = key;
-              tags.appendChild(tag);
-    
-              let backgroundColor;
-
-              switch (key) {
-                case 'JavaScript':
-                  backgroundColor = "#fdd692"; // Pastel yellow
-                  break;
-                case 'Python':
-                  backgroundColor = "#ff9980"; // Pastel red
-                  break;
-                case 'HTML':
-                  backgroundColor = "#ffafcc"; // Pastel pink
-                  break;
-                case 'C':
-                  backgroundColor = "#7fc8f8"; // Pastel blue
-                  break;
-                case 'C++':
-                  backgroundColor = "#a1e1d4"; // Pastel green
-                  break;
-                case 'Java':
-                  backgroundColor = "#a2cffe"; // Pastel sky blue
-                  break;
-                case 'Ruby':
-                  backgroundColor = "#ff9b9b"; // Pastel salmon
-                  break;
-                case 'CSS':
-                  backgroundColor = "#c7ceea"; // Pastel lavender
-                  break;
-                case 'PHP':
-                  backgroundColor = "#ffb2a7"; // Pastel coral
-                  break;
-                case 'Swift':
-                  backgroundColor = "#ffd06e"; // Pastel yellow-orange
-                  break;
-                case 'Go':
-                  backgroundColor = "#b5ffd9"; // Pastel mint
-                  break;
-                case 'Rust':
-                  backgroundColor = "#c9c9c9"; // Pastel gray
-                  break;
-                case 'TypeScript':
-                  backgroundColor = "#b8e9e5"; // Pastel turquoise
-                  break;
-                case 'Kotlin':
-                  backgroundColor = "#ffc0cb"; // Pastel rose
-                  break;
-                case 'Scala':
-                  backgroundColor = "#f6b4ee"; // Pastel orchid
-                  break;
-                case 'Perl':
-                  backgroundColor = "#f9cb9c"; // Pastel apricot
-                  break;
-                case 'Haskell':
-                  backgroundColor = "#c5ddeb"; // Pastel powder blue
-                  break;
-                case 'Lua':
-                  backgroundColor = "#ffc1a1"; // Pastel peach
-                  break;
-                case 'R':
-                  backgroundColor = "#e8b1b1"; // Pastel light pink
-                  break;
-                case 'Matlab':
-                  backgroundColor = "#ffe5b4"; // Pastel beige
-                  break;
-                default:
-                  backgroundColor = "#ffffff"; // White (default)
-              }
+        button.innerText = 'View Website';
 
 
-              tag.style.backgroundColor = backgroundColor;
-
-
-
-
-            }
-          }
-      
-      
-      } catch (error) {
-          console.error('Error:', error);
-      }
 
         // Append elements
         buttonWrapper.appendChild(button);
         projectBox.appendChild(projectTitle);
         projectBox.appendChild(projectDescription);
         projectBox.appendChild(tags);
-        projectBox.appendChild(buttonWrapper);
+
+        if (data[i].homepage != null & data[i].homepage != "") {
+          // Add a click event listener to the button
+          buttonWrapper.addEventListener("click", function() {
+          // Set the URL you want to navigate to
+          var url = data[i].homepage;
+
+          // Navigate to the URL
+          window.location.href = url;
+          });
+
+          projectBox.appendChild(buttonWrapper);
+        }
+        
 
         // Append projectBox to main element
         document.querySelector('#project-grid').appendChild(projectBox);
